0135ba5d3fc13fbabbd036d97e3ccf3032835867
Jisuk <muxacarin@users.noreply.github.com>
2024-02-08 09:59:33 +0900

    fix: change library name

--- lib/src/data_table/search_image_list.dart
+++ lib/src/data_table/search_image_list.dart
@@ -1,7 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
-import 'package:flutter_pinokio_package/flutter_pinokio_package.dart';
+import 'package:flutter_pinokio_library/flutter_pinokio_package.dart';
 
 class SearchImageList extends StatelessWidget {
   const SearchImageList(
